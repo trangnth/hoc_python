487380ebfddc45e300c85951c7cd0788a18619a4
trangnth <nguyenhuyentrang1996@gmail.com>
2018-07-31 14:34:59 +0700

    update

--- Code/Lab_python_basic/300718/get_infoOS.py
+++ Code/Lab_python_basic/300718/get_infoOS.py
@@ -1,39 +1,28 @@
 # Get info OS, RAM, CPU. print json 
 
-import platform, os, psutil
+import platform, psutil
 import json
 
 data = {}
-os = {}
-cpu = {}
-ram = {}
+_os = {}
+_cpu = {}
+_ram = {}
 
 def check_os():
-	os["platform"] = platform.platform()
-	os["system"] = platform.system()
-	os["node"] = platform.node()
-	os["release"] = platform.release()
-	os["version"] = platform.version()
+# check_distro	
+	data["distro"] = platform.platform()
 
-def check_cpu():
-	cpu["machine"] = platform.machine()
-	cpu["processor"] = platform.processor()
-	cpu["cpu_count:"] = psutil.cpu_count()
+# check_cpu():
+	data["cpu_count:"] = psutil.cpu_count()
 
-def check_ram():
-	ram["mem_total"] = psutil.virtual_memory().total
+# check_ram():
+	mem = psutil.virtual_memory().total / (1024**3)
+	data["mem_total"] = round(mem,2)
+
+check_os()
+print(data)
 
 #if __name__ == "__main__":
-check_os()
-check_ram()
-check_cpu()
-data["os"] = os
-data["cpu"] = cpu
-data["ram"] = ram
-print(data)
-#str = json.dumps(data,indent=2)
-#print(str)
-
 ### output file
 with open('infoOS.json', 'w') as f:
 	json.dump(data,f,indent=2)
