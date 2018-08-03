3d57ec8bbc4792b975dbe298e65d3d5301876747
trangnth <nguyenhuyentrang1996@gmail.com>
2018-08-03 15:49:22 +0700

    update 03/08/2018

--- Code/Lab_python_basic/300718/get_infoOS.py
+++ Code/Lab_python_basic/300718/get_infoOS.py
@@ -1,27 +1,25 @@
 # Get info OS, RAM, CPU. print json 
+# Sử dụng cho cả Windows và Ubuntu 
 
-import platform, psutil
-import json
+import platform, psutil, json
 
 data = {}
-_os = {}
-_cpu = {}
-_ram = {}
 
 def check_os():
-# check_distro	
-	data["distro"] = platform.platform()
+	# check_distro	
+	data["platform"] = platform.platform()
 
-# check_cpu():
+	# check_cpu():
 	data["cpu_count:"] = psutil.cpu_count()
 
-# check_ram():
+	# check_ram():
 	mem = psutil.virtual_memory().total / (1024**3)
 	data["mem_total"] = round(mem,2)
 	
 	print(data)
 
-	#if __name__ == "__main__":
 	### output file
 	with open('infoOS.json', 'w') as f:
 		json.dump(data,f,indent=2)
+
+#if __name__ == "__main__":
