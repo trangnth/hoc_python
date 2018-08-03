4d7d0742bd182e9a066faeac887f03f4d6db7744
trangnth <nguyenhuyentrang1996@gmail.com>
2018-08-03 17:09:31 +0700

    update 03/08/2018

--- Code/Lab_python_basic/300718/get_infoOS.py
+++ Code/Lab_python_basic/300718/get_infoOS.py
@@ -6,21 +6,21 @@ import psutil
 import json
 
 def info_os():
-	data = {}
-	# check_distro	
-	data["platform"] = platform.platform()
-	
-	# check_cpu():
-	data["cpu_count:"] = psutil.cpu_count()
+    data = {}
+    # check_distro  
+    data["platform"] = platform.platform()
+    
+    # check_cpu():
+        data["cpu_count:"] = psutil.cpu_count()
 
-	# check_ram():
-	mem = psutil.virtual_memory().total / (1024**3)
-	data["mem_total"] = round(mem,2)
-		
-	return data
+    # check_ram():
+    mem = psutil.virtual_memory().total / (1024**3)
+    data["mem_total"] = round(mem,2)
+        
+    return data
 
 ### output file
 #with open('infoOS.json', 'w') as f:
-#	json.dump(data,f,indent=2)
+#   json.dump(data,f,indent=2)
 
 #if __name__ == "__main__":
