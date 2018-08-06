53bedca9f923dffe323a9d3daaa1728d5f8e1a49
trangnth <nguyenhuyentrang1996@gmail.com>
2018-08-06 16:27:05 +0700

    update 06/08/2018

--- Code/Lab/pointClass/point.py
+++ Code/Lab/pointClass/point.py
@@ -10,33 +10,33 @@ Class gồm các method:
 from math import sqrt
 
 class Point:
-	
-	def __init__(self, x, y):
-		self.x=x
-		self.y=y
+    
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
 
-	def resetPoint(self):
-		self.x = 0
-		self.y = 0
+    def resetPoint(self):
+        self.x = 0
+        self.y = 0
 
-	def scale(self, a, b):
-		self.x *= a
-		self.y *= b
+    def scale(self, a, b):
+        self.x *= a
+        self.y *= b
 
-	def showPoint(self): 
-		print("x =", self.x)
-		print('y =',self.y)
+    def showPoint(self): 
+        print("x =", self.x)
+        print('y =',self.y)
 
-	def distancePoint(self, p):
-		return (sqrt((self.x - p.x)**2 + (self.y - p.y)**2))
+    def distancePoint(self, p):
+        return (sqrt((self.x - p.x)**2 + (self.y - p.y)**2))
 
-	def cosinPoint(self, d):
-		if self.x == d.x and self.y == d.y:
-			return False
-		elif self.x == d.x:
-			return 0
-		elif self.y == d.y:
-			return 1
-		else:
-			dist = sqrt((self.x - d.x)**2 + (self.y - d.y)**2)
-			return (abs(self.x - d.x) / dist)
+    def cosinPoint(self, d):
+        if self.x == d.x and self.y == d.y:
+            return False
+        elif self.x == d.x:
+            return 0
+        elif self.y == d.y:
+            return 1
+        else:
+            dist = sqrt((self.x - d.x)**2 + (self.y - d.y)**2)
+            return (abs(self.x - d.x) / dist)
