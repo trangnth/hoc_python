18bdc2a5dcc2d7a407a16564d2d5aceb913bd2ee
trangnth <nguyenhuyentrang1996@gmail.com>
2018-08-06 18:25:56 +0700

    update 06/08/2018

--- Code/Lab/class/point.py
+++ Code/Lab/class/point.py
@@ -32,7 +32,7 @@ class Point:
 
     def cosinPoint(self, d):
         if self.x == d.x and self.y == d.y:
-            return False
+            return "Diem trung nhau!"
         elif self.x == d.x:
             return 0
         elif self.y == d.y:
@@ -40,3 +40,22 @@ class Point:
         else:
             dist = sqrt((self.x - d.x)**2 + (self.y - d.y)**2)
             return (abs(self.x - d.x) / dist)
+
+def main():
+    p1 = Point(1,1)
+    p2 = Point(4,3)
+    p3 = Point(3,4)
+
+    p1.showPoint()
+
+    p2.scale(2,3)
+    p2.showPoint()
+
+    p1.resetPoint()
+    p1.showPoint()
+
+    print(p2.distancePoint(p3))
+    print(p1.cosinPoint(p2))
+
+if __name__ == "__main__":
+    main()
---
+++ Code/Lab/class/rectangle.py
@@ -0,0 +1,59 @@
+from point import Point
+
+class Rectangle :
+	width = 0
+	height = 0
+	def __init__(self, p1, p2, p3, p4):       
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+
+
+    def isRect(self):
+    	if self.p1 == self.p2 or self.p1 == self.p3 or self.p1 == self.p4 or self.p2 == self.p3 or self.p2 == self.p4 or self.p3 == self.p4:
+    		return False
+    	elif self.p1.x == self.p4.x and self.p1.y == self.p2.y and self.p2.x == self.p3.x and self.p3.y == self.p4.y:
+    		
+    		if (self.p2.x - self.p1.x) >= (self.p1.y - self.p4.y):
+		    	self.width = self.p2.x - self.p1.x
+		    	self.height = self.p1.y - self.p4.y
+		    else:
+		    	self.width = self.p1.y - self.p4.y
+		    	self.height = self.p2.x - self.p1.x
+		    return True
+    	else:
+    		print(p4.x)
+    		return False
+
+    def getWidth(self):
+    	return self.width
+
+    def getHeight(self):
+    	return self.height
+
+    def areaRect(self):
+        return self.width * self.height
+
+    def perimeterRect(self):
+    	return (self.width + self.height)*2
+
+def main():
+	# 4 diem khai bao lan luot theo thu tu chieu kim dong ho
+    p1 = Point(1,4)
+    p2 = Point(5,4)
+    p3 = Point(5,2)
+    p4 = Point(1,2)
+    r1 = Rectangle(p1, p2, p3, p4)
+
+    if r1.isRect() == True:
+    	print ("width =", r1.getWidth())
+    	print("height =", r1.getHeight())
+    	print("Dien tich: ", r1.areaRect())
+    	print("Chu vi: ", r1.perimeterRect())
+
+    else:
+    	print("r1 is not rectangle.")
+
+if __name__ == "__main__":
+    main()
